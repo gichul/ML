87947f5b9553539be1c890752dc7d9b149e7c797
gichul <kgc5551@naver.com>
2019-04-30 17:09:07 +0900

    added

---
+++ ML_lab_11/same.py
@@ -0,0 +1,10 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+image = np.array([[[[4],[3]],
+                    [[2],[1]]]], dtype=np.float32)
+pool = tf.nn.max_pool(image, ksize=[1, 2, 2, 1],
+                    strides=[1, 1, 1, 1], padding='SAME')
+print(pool.shape)
+print(pool.eval())
