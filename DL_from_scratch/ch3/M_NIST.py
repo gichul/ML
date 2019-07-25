6231100ae9e9e53108ecb18e33fe65383e6734d4
gichul <kgc5551@naver.com>
2019-07-25 23:05:39 +0900

    added

--- DL_from_scratch/ch3/M_NIST.py
+++ DL_from_scratch/ch3/M_NIST.py
@@ -3,18 +3,20 @@ sys.path.append(os.pardir)
 from mnist import load_mnist
 import numpy as np
 from PIL import Image
+from processing import *
 
 def img_show(img):
 	pil_img=Image.fromarray(np.uint8(img))
 	pil_img.show()
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True,normalize=False)
-'''
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
-'''
+
+	
+#print(x_train.shape)
+#print(t_train.shape)
+#print(x_test.shape)
+#print(t_test.shape)
+
 img =x_train[0]
 label=t_train[0]
 print(label)
@@ -26,4 +28,15 @@ print(img.shape)
 img_show(img)
 
 
+x,y=get_data()
+#print(x[0])
+img=x[1000]
+img=img.reshape(28,28)
+print(img.shape)
+img_show(img)
 
+label=y[1000]
+print(label)
+
+
+#accuracy()
